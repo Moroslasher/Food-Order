8252a4703b6b2fc1f41bc616dad74aab68a593c0
Moroslasher <moroslasher0014@gmail.com>
2026-03-14 18:36:34 -0700

    Create foodorder.py

---
+++ foodorder.py
@@ -0,0 +1,22 @@
+print("ORDER PROMPT")
+print(2023 Moroslasher")
+order = input("Welcome to [RESTAURANT], can I take your order?")
+
+if(order == "salad"):
+  print("Great!")\
+  print("*hands customer salad*")
+
+elif(order == "hamburger"):
+  print("Alrighty!")
+  print("*hands customer hamburger*")
+
+elif(order == "cheeseburger"):
+  print("Great!")
+  print("*hands customer cheeseburger*")
+
+elif(order == "portal to hell\'94):
+  print("I see.")\
+  print("*banishes you to the deepest part of hell*")
+
+else:
+  print("Get lost!")
